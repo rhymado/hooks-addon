fd5d18c8164793db43cbf6668fad173d54e45bcf
Fakhri <fakhridho95@gmail.com>
2021-04-30 15:38:55 +0700

    useeffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
   // [value, setValue] = useState(initialValue)
-  const [teks, setTeks] = useState("");
-  console.log(teks);
+  const [username, setUsername] = useState("");
+  const [order, setOrder] = useState(0);
+  useEffect(() => {
+    console.log("use Effect");
+  }, [username, order]);
+  // [] => equivalent dengan componendDidMount
+  // [var] => equivalent dengan componentDidUpdate
+  // undefined => dijalankan setiap rerender
   return (
     <>
       <div>Home</div>
       <input
         type="text"
-        name="teks"
-        value={teks}
+        name="username"
+        value={username}
         onChange={(e) => {
           const input = e.target.value;
           if (
-            input.charCodeAt(input.length - 1) >= 48 &&
-            input.charCodeAt(input.length - 1) <= 57
-          ) {
-            return;
-          } else {
-            setTeks(input);
-          }
+            input.charCodeAt(input.length - 1) < 48 ||
+            input.charCodeAt(input.length - 1) > 57
+          )
+            return setUsername(input);
         }}
       />
-      {/* <p>{teks}</p> */}
+      <input
+        type="number"
+        name="order"
+        value={order}
+        onChange={(e) => setOrder(e.target.value)}
+      />
     </>
   );
 }
