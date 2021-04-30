cd07dd948f52e0cedf929e1de977cc42a1b6c67c
Fakhri <fakhridho95@gmail.com>
2021-04-30 15:29:25 +0700

    usestate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  // [value, setValue] = useState(initialValue)
+  const [teks, setTeks] = useState("");
+  console.log(teks);
+  return (
+    <>
+      <div>Home</div>
+      <input
+        type="text"
+        name="teks"
+        value={teks}
+        onChange={(e) => {
+          const input = e.target.value;
+          if (
+            input.charCodeAt(input.length - 1) >= 48 &&
+            input.charCodeAt(input.length - 1) <= 57
+          ) {
+            return;
+          } else {
+            setTeks(input);
+          }
+        }}
+      />
+      {/* <p>{teks}</p> */}
+    </>
+  );
 }
