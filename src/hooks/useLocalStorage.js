7f694695f3846a3471043e291d264f5330998be3
Fakhri <fakhridho95@gmail.com>
2021-04-30 16:39:03 +0700

    update custom hooks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -8,17 +8,19 @@ export default function useLocalStorage(key, initialValue) {
     // jika key sudah ada di localstorage, maka gunakan value dari localstorage
     // jika key tidak ada di localstorage, maka gunakan initialvalue lalu simpan ke localstorage
     const jsonValue = localStorage.getItem(key);
+    // console.log(typeof jsonValue);
     if (jsonValue !== null) return JSON.parse(jsonValue);
     // initial value bisa berupa fungsi ataupun non-fungsi
     // jika fungsi, maka jalankan
     // jika bukan, maka return
-    if (typeof initialValue === "function") return initialValue();
-    return initialValue;
+    if (typeof initialValue === "function")
+      return JSON.stringify(initialValue());
+    return JSON.stringify(initialValue);
   });
 
   // jika value berubah, maka update localstorage
   useEffect(() => {
-    localStorage.setItem(key, value);
+    localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
   return [value, setValue];
